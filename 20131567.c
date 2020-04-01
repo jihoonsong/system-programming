ea854b8a9eefd1f427b8ea6412fbff8c54558133
JihoonSong <jihoon1319@gmail.com>
2020-04-02 03:32:05 +0900

    Add cmd, argc, and argv variables
    After tokenizing the input, we get three tokens; cmd, argc, and
    argv. According to the specificaiton, the number of argc can be 3
    at most. This value is set one bigger than it in order to sense if
    there are arguments more than expected.
    
    Resolves: #2

--- 20131567.c
+++ 20131567.c
@@ -8,10 +8,41 @@
 #include <stdlib.h>
 
 /**
+ * @def   ARGC_MAX
+ * @brief The maximum value of argc. According to the specificaiton,
+ *        the number of argc can be 3 at most. This value is set one
+ *        bigger than it in order to sense if there are arguments more
+ *        than expected.
+ * @see   argc
+ */
+#define ARGC_MAX 4
+
+/**
+ * @def   INPUT_LEN
  * @brief The length of input string. Its value can be modified
  *        if it is long enough to hold command and arguments.
+ * @see   input
  */
 #define INPUT_LEN 64
+
+/**
+ * @brief The number of arguments. Its maximum value is ARGC_MAX.
+ * @see   ARGC_MAX
+ * @see   argv
+ */
+static int argc = 0;
+
+/**
+ * @brief An list of arguments with trailing NULL.
+ * @see   ARGC_MAX
+ * @see   argc
+ */
+static char *argv[ARGC_MAX + 1] = {NULL,};
+
+/**
+ * @brief A pointer to an array of char that contains command.
+ */
+static char *cmd = NULL;
 
 /**
  * @brief An array of char that holds received command and arguments.
