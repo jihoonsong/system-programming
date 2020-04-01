ec91f3ae088bb51501fd906a7aba554f3b71c5b8
JihoonSong <jihoon1319@gmail.com>
2020-04-01 22:37:05 +0900

    Add a main source file
    This file is a mainloop component. It does three things;
    initialize states, start main loop, and clean up memory before
    this program is over.
    
    According to the project specification, the filename is named
    after my student ID.
    
    Resolves: #1

---
+++ 20131567.c
@@ -0,0 +1,17 @@
+/**
+ * @file 20131567.c
+ * @brief The starting point of this program.
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief Initialize states and start main loop.
+ *        Clean up memory when the main loop is over.
+ */
+int main(void)
+{
+  printf("hello world\n");
+
+  return 0;
+}
