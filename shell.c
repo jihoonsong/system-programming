c5e3ec710693e87d12bf76fa53ab76a198833030
JihoonSong <jihoon1319@gmail.com>
2020-04-04 01:09:41 +0900

    Add shell_execute()
    It receives and execute command.
    
    Resolves: #3

--- shell.c
+++ shell.c
@@ -2,3 +2,8 @@
  * @file  shell.h
  * @brief A handler of shell related commands.
  */
+
+void shell_execute(char *cmd, int argc, char *argv[])
+{
+  // TODO: to be implemented.
+}
--- shell.h
+++ shell.h
@@ -6,5 +6,12 @@
 #ifndef __SHELL_H__
 #define __SHELL_H__
 
+/**
+ * @brief          Receives command and executes the command.
+ * @param[in] cmd  A type of the command.
+ * @param[in] argc The number of arguments.
+ * @param[in] argv An list of arguments.
+ */
+void shell_execute(char *cmd, int argc, char *argv[]);
 
 #endif
