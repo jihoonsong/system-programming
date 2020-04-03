6d08cd44d146f3112102d1b7d144d525eb333cdb
JihoonSong <jihoon1319@gmail.com>
2020-04-03 13:03:39 +0900

    Add mainloop.{c,h}
    Separate mainloop from main.c file.
    
    mainloop is like a client of command pattern. It has all information
    need to execute command such as command, arguments, the number of
    arguments, and handler.
    
    mainloop receives command and then execute it by invoking handler.
    
    Resolves: #4

---
+++ mainloop.c
@@ -0,0 +1,5 @@
+/**
+ * @file  mainloop.c
+ * @brief A mainloop of this program. Acts like a client of
+ *        command pattern. Receives command and invokes handler.
+ */
---
+++ mainloop.h
@@ -0,0 +1,11 @@
+/**
+ * @file  mainloop.h
+ * @brief A mainloop of this program. Acts like a client of
+ *        command pattern. Receives command and invokes handler.
+ */
+
+#ifndef __MAINLOOP_H__
+#define __MAINLOOP_H__
+
+
+#endif
