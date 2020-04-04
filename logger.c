1d76fa6c2a84fe50988d5e657da18b8ec18b03ca
JihoonSong <jihoon1319@gmail.com>
2020-04-04 19:02:04 +0900

    Add data structure of log in logger
    Logs are stored in list.
    
    Resolves: #5

--- logger.c
+++ logger.c
@@ -5,6 +5,27 @@
 
 #include <stdio.h>
 
+/**
+ * @brief An element of log list.
+ */
+struct log
+{
+  /** A pointer to the next log element. */
+  struct log *next;
+  /** A string of executed command. It has a compact length */
+  char       command[];
+};
+
+/**
+ * @brief A pointer to a head of log list.
+ */
+struct log *_log_head;
+
+/**
+ * @brief A pointer to a tail of log list.
+ */
+struct log *_log_tail;
+
 void logger_initialize(const int input_length)
 {
   // TODO: to be implemented.
