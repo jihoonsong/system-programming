1a31537a5853ad6696b24520cc6e00e59f5ec6da
JihoonSong <jihoon1319@gmail.com>
2020-04-04 21:02:55 +0900

    Implement test code for logger_write_log()
    It calls logger_write_log() with valid command and test if a new log
    is written correctly.
    
    Resolves: #5

--- unit_test/test_logger.c
+++ unit_test/test_logger.c
@@ -3,11 +3,106 @@
  * @brief Test functions for logger.c.
  */
 
+#include <string.h>
+
 #include "../logger.c"
 
 #include "test_logger.h"
 
+/**
+ * @def   ARGC_MAX
+ * @brief The maximum value of argc. According to the specificaiton,
+ *        the number of argc can be 3 at most. This value is set one
+ *        bigger than it in order to sense if there are arguments more
+ *        than expected.
+ * @see   argc
+ */
+#define ARGC_MAX 4
+
+/**
+ * @brief Structure of command elements and expected log.
+ */
+struct test_log
+{
+  char *cmd;
+  int  argc;
+  char *argv[ARGC_MAX + 1];
+  char *command;
+};
+
+/**
+ * @brief The number of failed test cases.
+ */
+static int _fail_count = 0;
+
+/**
+ * @brief The number of passed test cases.
+ */
+static int _pass_count = 0;
+
+/**
+ * @brief Test cases.
+ */
+struct test_log _test_logs[] = \
+  {(struct test_log){.cmd = "h",
+                     .argc = 0,
+                     .argv = {NULL},
+                     .command = "h"},
+  (struct test_log){.cmd = "history",
+                    .argc = 0,
+                    .argv = {NULL},
+                    .command = "history"},
+  (struct test_log){.cmd = "du",
+                    .argc = 2,
+                    .argv = {"12", "3F"},
+                    .command = "du 12, 3F"},
+  (struct test_log){.cmd = "edit",
+                    .argc = 2,
+                    .argv = {"23", "DD"},
+                    .command = "edit 23, DD"},
+  (struct test_log){.cmd = "fill",
+                    .argc = 3,
+                    .argv = {"4", "37", "1D"},
+                    .command = "fill 4, 37, 1D"}};
+
+/**
+ * @brief The number of test cases.
+ */
+static int _test_log_count = (int)(sizeof(_test_logs) /
+                                   sizeof(_test_logs[0]));
+
+/**
+ * @brief             Test logger_write_log().
+ * @param[in] command Test will be done on this.
+ */
+static void test_logger_write_log(struct test_log *log);
+
 void test_logger(void)
 {
-  // TODO: to be implemented.
+  for(int i = 0; i < _test_log_count; ++i)
+  {
+    test_logger_write_log(&_test_logs[i]);
+  }
+
+  printf("\n");
+  printf("Pass: %d\n", _pass_count);
+  printf("Fail: %d\n", _fail_count);
+}
+
+static void test_logger_write_log(struct test_log *log)
+{
+  printf("Test test_logger_write_log() with '%s': ", log->command);
+
+  logger_write_log(log->cmd, log->argc, log->argv);
+
+  if(!strcmp(log->command, _log_tail->command))
+  {
+    ++_pass_count;
+    printf("pass.\n");
+  }
+  else
+  {
+    ++_fail_count;
+    printf("fail. it should be '%s', not '%s'.\n", log->command, _log_tail->command);
+  }
 }
