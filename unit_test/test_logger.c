8ef220d5afe0b58e20341654472d404726061ac6
JihoonSong <jihoon1319@gmail.com>
2020-04-04 19:00:29 +0900

    Add test file for logger
    This file has test code for logger. logger_write_log() will be tested.
    
    Resolves: #5

---
+++ unit_test/test_logger.c
@@ -0,0 +1,13 @@
+/**
+ * @file  test_logger.c
+ * @brief Test functions for logger.c.
+ */
+
+#include "../logger.c"
+
+#include "test_logger.h"
+
+void test_logger(void)
+{
+  // TODO: to be implemented.
+}
