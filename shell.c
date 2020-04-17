770c130f54e6f1e06a8c75c2b93e69a06e29af04
JihoonSong <jihoon1319@gmail.com>
2020-04-17 14:09:25 +0900

    Modify help command
    Following new commands will be added, so help command need to be
    modified accordingly. The new commands are assemble filename,
    type filename, and symbol.
    
    Resolves: #8

--- shell.c
+++ shell.c
@@ -142,6 +142,9 @@ static bool shell_execute_help(const char *cmd, const int argc, const char *argv
   printf("reset\n");
   printf("opcode mnemonic\n");
   printf("opcodelist\n");
+  printf("assemble filename\n");
+  printf("type filename\n");
+  printf("symbol\n");
 
   return true;
 }
