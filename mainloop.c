10b675b6ac47f23b12135ad150be9ff8bd797f66
JihoonSong <jihoon1319@gmail.com>
2020-04-17 15:34:36 +0900

    Replace external variables with static variables

--- mainloop.c
+++ mainloop.c
@@ -45,7 +45,7 @@ struct command
 /**
  * @brief A const variable that holds the length of input.
  */
-const int INPUT_LEN = 64;
+static const int INPUT_LEN = 64;
 
 /**
  * @brief A command object that contains all information need to
