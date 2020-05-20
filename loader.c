b0e336c3e5f99e4d4165595baf2167ff03809802
JihoonSong <jihoon1319@gmail.com>
2020-05-20 11:50:09 -0700

    Add loader_execute_loader() at loader handler
    loader_execute_loader() performs linking and loading.
    
    Resolves: #10

--- loader.c
+++ loader.c
@@ -5,6 +5,7 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "loader.h"
 
@@ -15,14 +16,51 @@
  */
 static bool _is_command_executed = false;
 
+/**
+ * @brief          Perform linking and loading.
+ * @param[in] cmd  A type of the command.
+ * @param[in] argc The number of arguments.
+ * @param[in] argv An list of arguments.
+ */
+static bool loader_execute_loader(const char *cmd,
+                                  const int  argc,
+                                  const char *argv[]);
+
 void loader_execute(const char *cmd,
                     const int  argc,
                     const char *argv[])
 {
-  printf("loader received %s\n", cmd);
+  if(!strcmp("loader", cmd))
+  {
+    _is_command_executed = loader_execute_loader(cmd, argc, argv);
+  }
+  else
+  {
+    printf("%s: command not found\n", cmd);
+  }
 
   if(_is_command_executed)
   {
     logger_write_log(cmd, argc, argv);
   }
+}
+
+static bool loader_execute_loader(const char *cmd,
+                                  const int  argc,
+                                  const char *argv[])
+{
+  if(0 == argc)
+  {
+    printf("loader: at least one object file is required\n");
+    return false;
+  }
+  if(3 < argc)
+  {
+    printf("loader: at most three object files can be loaded\n");
+    return false;
+  }
+
+  printf("loader_execute_loader is called\n");
+
+  return true;
 }
