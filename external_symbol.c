895d85de030bf0ad3beb5dedaed267280acb3f0b
JihoonSong <jihoon1319@gmail.com>
2020-05-20 23:21:12 -0700

    Add external symbol handler
    The external symbol handler is responsible an external symbol table
    used during linking and loader. It behaves like symbol handler.
    
    Resolves: #10

---
+++ external_symbol.c
@@ -0,0 +1,11 @@
+/**
+ * @file  external_symbol.c
+ * @brief A external symbol table used during linking and loading.
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "external_symbol.h"
---
+++ external_symbol.h
@@ -0,0 +1,10 @@
+/**
+ * @file  external_symbol.h
+ * @brief A external symbol table used during linking and loading.
+ */
+
+#ifndef __EXTERNAL_SYMBOL_H__
+#define __EXTERNAL_SYMBOL_H__
+
+
+#endif
